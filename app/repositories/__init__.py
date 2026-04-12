45a2959474e3f2339c269d9e113aa8e178ff0a8e
SaifBoodoo <sboodoo659@gmail.com>
2026-04-12 19:16:29 -0400

    fixed init.py

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,1 +1,14 @@
 from .user import UserRepository
+from .student import StudentRepository
+from .company import CompanyRepository
+from .project import ProjectRepository
+from .application import ApplicationRepository
+
+# This optionally tells Python what to export when someone runs 'from app.repositories import *'
+__all__ = [
+    "UserRepository",
+    "StudentRepository",
+    "CompanyRepository",
+    "ProjectRepository",
+    "ApplicationRepository"
+]
